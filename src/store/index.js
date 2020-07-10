18d78d4fe41e1ef3c6d9010520ce0a7eee315779
Ariene Ribeiro <aribeiro@img.com.br>
2020-07-10 17:36:46 -0300

    Adding redux saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,17 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "./modules/rootReducer";
+import rootSaga from "./modules/rootSaga";
 
+const sagaMiddleware = createSagaMiddleware();
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(sagaMiddleware))
 );
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
